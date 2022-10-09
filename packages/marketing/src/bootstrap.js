70692d84e848bce87f7c78e805af72849b67e323
jpeg25 <jairo25509@hotmail.com>
2022-10-09 11:54:08 -0500

    trigger marketing deploy

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -8,7 +8,7 @@ const mount = (el) => {
 };
 
 // If we are in development and in isolation,
-// call mount immediately
+// call mount immediately.
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
